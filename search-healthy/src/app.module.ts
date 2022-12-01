294a62361c0fce5d7dfe0d709d3cc07dbe18b25a
Iuri Dantas <uri.dantas@hotmail.com>
2022-12-01 19:53:53 -0300

    feat: create base structure registration

--- search-healthy/src/app.module.ts
+++ search-healthy/src/app.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from './prisma/database.module';
 import { UserService } from './user/service/user.service';
 import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
+import { RegistrationModule } from './registration/registration.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RegistrationModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
