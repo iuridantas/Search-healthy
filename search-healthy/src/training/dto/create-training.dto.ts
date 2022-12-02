27ae56cf27515a73984ce30b63b3f87998dfc0b5
Iuri Dantas <uri.dantas@hotmail.com>
2022-12-01 21:43:23 -0300

    feat: implement training entity

--- search-healthy/src/training/dto/create-training.dto.ts
+++ search-healthy/src/training/dto/create-training.dto.ts
@@ -1,1 +1,8 @@
-export class CreateTrainingDto {}
+export class CreateTrainingDto {
+  diceId: string;
+  day: Date;
+  exercise: string;
+  repetition: number;
+  aerobic: number;
+  stretching: string;
+}
--- search-healthy/src/training/entities/training.entity.ts
+++ search-healthy/src/training/entities/training.entity.ts
@@ -1,1 +1,5 @@
-export class Training {}
+import { CreateTrainingDto } from '../dto/create-training.dto';
+
+export class Training extends CreateTrainingDto {
+  id: string;
+}
