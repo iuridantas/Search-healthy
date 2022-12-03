3e8382a527598f3630ae681dbd1847efe6bb3b46
Iuri Dantas <uri.dantas@hotmail.com>
2022-12-03 14:15:10 -0300

    feat: create base structure profile

--- search-healthy/src/app.module.ts
+++ search-healthy/src/app.module.ts
@@ -5,9 +5,10 @@ import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
 import { TrainingModule } from './training/training.module';
 import { DiceModule } from './dice/dice.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [DatabaseModule, TrainingModule, DiceModule],
+  imports: [DatabaseModule, TrainingModule, DiceModule, ProfileModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
